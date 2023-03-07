4277ee0296d16a270c87baefba8da9a2329746d0
Jahaziel Hernandez Hoyos <jahazielhernandezhoyoz@gmail.com>
2023-03-06 23:51:31 -0500

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI, File, UploadFile
-from api.endpoints import processing_images
+from fastapi import FastAPI, File, UploadFile, Request
+from api.endpoints import processing_images, processing_text
 from utils.save_image_temporal import save_in_disk
 from io import BytesIO
-import os
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
@@ -12,6 +12,13 @@ async def call_processing_images(image: UploadFile = File(...)):
     image_path = save_in_disk(image_bytes)
     result = await processing_images.processing_images(image_path)
     return result
+
+@app.post("/processing_text")
+def call_processing_text(tex: str, request: Request):
+    result = processing_text.processing_text(tex, request)
+    return result
+
+app.mount("/static", StaticFiles(directory="utils/abc"), name="static")
 
 @app.get("/")
 async def root():

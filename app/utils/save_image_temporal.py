fe0a3a810da8178ba7c3826993e4ddf9cf2600db
Jahaziel Hernandez Hoyos <jahazielhernandezhoyoz@gmail.com>
2023-09-28 12:04:20 -0500

    Update save_image_temporal.py
    Add documentation in the save image temporal

--- app/utils/save_image_temporal.py
+++ app/utils/save_image_temporal.py
@@ -1,6 +1,7 @@
 import os
 
 def save_in_disk(image_bytes: bytes) -> str:
+    """save images in the server"""
     filename = "temp.jpg"
     with open(filename, "wb") as f:
         f.write(image_bytes)
